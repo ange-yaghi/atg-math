075e97c2cb284a61a4f69c0fac11b4383a2fb7ea
Ange Yaghi <me@angeyaghi.com>
2024-08-12 14:35:30 -0400

    Updated constants

--- include/constants.h
+++ include/constants.h
@@ -6,16 +6,20 @@
 #define ATG_MATH_E_DIGITS                                                      \
     2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274
 
+#define ATG_MATH_CONSTANTS_CONSTANT(name, value)                               \
+    template<typename T_Return = double>                                       \
+    inline constexpr T_Return name() {                                         \
+        return static_cast<T_Return>((value));                                 \
+    }                                                                          \
+    constexpr double name##_d = name();                                        \
+    constexpr float name##_f = name<float>();
+
 namespace atg_math {
 namespace constants {
 
-constexpr float pi_f = float(ATG_MATH_PI_DIGITS);
-constexpr double pi_d = double(ATG_MATH_PI_DIGITS);
-constexpr double pi = pi_d;
-
-constexpr float e_f = float(ATG_MATH_E_DIGITS);
-constexpr double e_d = double(ATG_MATH_E_DIGITS);
-constexpr double e = e_d;
+ATG_MATH_CONSTANTS_CONSTANT(pi, ATG_MATH_PI_DIGITS);
+ATG_MATH_CONSTANTS_CONSTANT(tau, ATG_MATH_PI_DIGITS * 2.0L);
+ATG_MATH_CONSTANTS_CONSTANT(e, ATG_MATH_E_DIGITS);
 
 }// namespace constants
 }// namespace atg_math
