bd49b358e8a728bae55ab3a00518bb403cf531b6
Ange Yaghi <me@angeyaghi.com>
2026-03-16 11:58:01 -0400

    Adds ray-plane intersection

--- include/atg-math/intersection.hpp
+++ include/atg-math/intersection.hpp
@@ -12,6 +12,14 @@ T_Vec linePlaneIntersection(const T_Vec &l0, const T_Vec &l, const T_Vec &p0,
     return l0 + l * d;
 }
 
+template<typename T_Vec>
+T_Vec rayPlaneIntersection(const T_Vec &l0, const T_Vec &l, const T_Vec &p0,
+                           const T_Vec &n, bool *intersects) {
+    const T_Vec d = (p0 - l0).dot(n) / (l.dot(n));
+    *intersects = bool(d > T_Vec(0));
+    return l0 + l * d;
+}
+
 }// namespace atg_math
 
 #endif /* ATG_ATG_MATH_INTERSECTION_HPP */
